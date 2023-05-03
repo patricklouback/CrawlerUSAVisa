8e9002aace4acd59050942afa38e4bed42f4470c
Patrick Louback <patrickmlouback@gmail.com>
2023-05-03 13:24:36 -0300

    inserindo função que itera a tabela para buscar

--- interactions.py
+++ interactions.py
@@ -1,9 +1,14 @@
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+def wait_for_page_to_load(driver):
+    wait = WebDriverWait(driver, 30)
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    wait.until(lambda driver: driver.execute_script("return document.readyState") == "complete")
 
 def wait_for_element_presence(driver, xpath, timeout=30):
     try:
@@ -13,10 +18,46 @@ def wait_for_element_presence(driver, xpath, timeout=30):
         print(f"O elemento com xpath {xpath} não foi encontrado na página após {timeout} segundos de espera.")
 
 def click_element(driver, xpath):
+    wait_for_page_to_load(driver)
     wait_for_element_presence(driver, xpath)
     driver.find_element(By.XPATH, xpath).click()
 
 def write_element(driver, xpath, msg):
+    wait_for_page_to_load(driver)
     elem = driver.find_element(By.XPATH, xpath)
     elem.clear()
     elem.send_keys(msg)
+
+def read_attribute(driver, attribute, tag, text_element):
+    wait_for_page_to_load(driver)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find(tag, string= text_element)
+    att = element[attribute]
+    return att
+
+def read_element(driver, tag, class_element):
+    wait_for_page_to_load(driver)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    element = soup.find(tag, class_=class_element)
+    return element
+
+def find_first_available_day(driver, xpath_table, xpath_month, xpath_year):
+    wait_for_page_to_load(driver)
+
+    table = driver.find_element(By.XPATH, xpath_table)
+    month = driver.find_element(By.XPATH, xpath_month).text
+    year = driver.find_element(By.XPATH, xpath_year).text
+
+    days = table.find_elements(By.TAG_NAME, 'td')
+    first_available_day = None
+    for day in days:
+        if not day.get_attribute('class') or 'ui-state-disabled' not in day.get_attribute('class'):
+            first_available_day = day.text
+            break
+    
+    if first_available_day is None:
+        return first_available_day
+    else:
+        return first_available_day + '/' + month + '/' + year
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ password = os.environ.get('PASSWORD')
 
 
 def crawler():
+    url_base = 'https://ais.usvisa-info.com'
     url = 'https://ais.usvisa-info.com/pt-br/niv/users/sign_in'
     driver = webdriver.Chrome()
     driver.maximize_window()
@@ -28,26 +29,47 @@ def crawler():
     time.sleep(0.2)
     write_element(driver, xpath.USER_PASSWORD, password)
     time.sleep(0.2)
+
     click_element(driver, xpath.POLICY_CONFIRMED)
     time.sleep(0.2)
+
     click_element(driver, xpath.ENTER_LOGIN)
     time.sleep(0.2)
+
     click_element(driver, xpath.CONTINUE)
     time.sleep(0.2)
+
     click_element(driver, xpath.SCHEDULE_LIST)
     time.sleep(0.2)
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    element = soup.find('a', string='Reagendar entrevista')
-    href = element['href']
-    driver.get('https://ais.usvisa-info.com' + href)
+    href = read_attribute(driver, 'href', 'a', 'Reagendar entrevista')
+    driver.get(url_base + href)
+    time.sleep(0.2)
 
     click_element(driver, xpath.CONTINUE_1)
-    time.sleep(0.5)
+    time.sleep(0.2)
 
     click_element(driver, xpath.DATE_APPOINTMENT)
-    time.sleep(5)
+    time.sleep(0.2)
+
+    day_table = find_first_available_day(driver, xpath.TABLE_PART_1, xpath.MONTH_DIV_1, xpath.YEAR_DIV_1)
+    print('Table 1',day_table)
+    print(type(day_table))
+
+    day_table = find_first_available_day(driver, xpath.TABLE_PART_2, xpath.MONTH_DIV_2, xpath.YEAR_DIV_2)
+    print('Table 2',day_table)
+    print(type(day_table))
+
+    day_table = None
+
+    while day_table == None:
+        click_element(driver, xpath.NEXT_CALENDAR)
+        time.sleep(0.2)
+        day_table = find_first_available_day(driver, xpath.TABLE_PART_2, xpath.MONTH_DIV_2, xpath.YEAR_DIV_2)
+        print('Table 2',day_table)
+        print(type(day_table))
+
+
 
     time.sleep(5)
 
